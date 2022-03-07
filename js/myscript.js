0be48edff07f43325e0cad0fb7a601e64b573a13
tarynmh <70242633+tarynmh@users.noreply.github.com>
2022-03-06 19:38:18 -0700

    Mobile-friendly Updates
    - Added responsive features (i.e. hamburger menu nav bar)
    - General design revisions

---
+++ js/myscript.js
@@ -0,0 +1,11 @@
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+// referenced https://www.w3schools.com/howto/howto_js_topnav_responsive.asp
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
