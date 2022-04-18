3e38a8f5a5a5618e72c5ba5b4d51bdceb03f4d65
thovenkamp <56414456+thovenkamp@users.noreply.github.com>
2022-04-17 20:45:58 -0600

    updated assignment 13

---
+++ demos/week12-demo.js
@@ -0,0 +1,10 @@
+// document.querySelector('button');
+// document.querySelector('.new-quote button');
+
+const quoteButton = document.querySelector('#js-new-quote');
+
+quoteButton.addEventListener('click', getQuote);
+
+function getQuote() {
+    
+}
